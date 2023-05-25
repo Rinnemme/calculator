2012eeaccc671517d026bcad1fb761d130bebf0a
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-05-25 16:35:36 -0600

    jot down pseudocode for math functions that I *think* works

---
+++ scripts.js
@@ -0,0 +1,30 @@
+/* When number is hit
+    if operator is none
+        display.textContent = display.textContent + this.textContent
+        firstOperand = display.textContent
+    if operator is something
+        display.textContent = display.textContent + this.textContent
+        secondOperand = display.textContent
+When operator is hit
+    if operator is none
+        display.textContent = cleared (nothing)
+        operator = times (or plus or minus or divide)
+    if operator is something
+        equals function
+        operator = times (or plus or minus or equals)
+If equals is hit
+    equals function:
+    if operator is none
+        result = firstOperand
+    if operator is something
+        equals(firstOperand, secondOperand) {
+            if operator is times, const result = number(firstOperand) * number(secondOperand)
+            else if operator is plus, const result = number(firstOperand) + number(secondOperand)
+            else if operator is minus, const result = number(firstOperand) - number(secondOperand)
+            else if operator is divided-by, const result = number(firstOperand) / number(secondOperand)
+            display.textContent = result
+            firstOperand = result
+            operator = none
+    } 
+    
+    */
