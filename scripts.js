d7abb3958bdb778a56f34f8ba9b620bb25a0e5cd
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2023-05-28 17:21:26 -0600

    clean up code, remove psuedocode and rounding code in solve function

--- scripts.js
+++ scripts.js
@@ -1,33 +1,4 @@
-/* When number is hit
-    if operator is none
-        display.textContent = display.textContent + this.textContent
-        firstOperand = display.textContent
-    if operator is something
-        display.textContent = display.textContent + this.textContent
-        secondOperand = display.textContent
-When operator is hit
-    if operator is none
-        display.textContent = cleared (nothing)
-        operator = times (or plus or minus or divide)
-    if operator is something
-        equals function
-        operator = times (or plus or minus or equals)
-If equals is hit
-    equals function:
-    if operator is none
-        result = firstOperand
-    if operator is something
-        equals(firstOperand, secondOperand) {
-            if operator is times, const result = number(firstOperand) * number(secondOperand)
-            else if operator is plus, const result = number(firstOperand) + number(secondOperand)
-            else if operator is minus, const result = number(firstOperand) - number(secondOperand)
-            else if operator is divided-by, const result = number(firstOperand) / number(secondOperand)
-            display.textContent = result
-            firstOperand = result
-            operator = none
-    } 
-    
-    */
+/* Define our variables */
 
 const numberButtons = document.querySelectorAll(".number")
 const operatorButtons = document.querySelectorAll(".operator")
@@ -38,36 +9,7 @@ let firstOperand = ""
 let secondOperand = ""
 let result = ""
 
-function solve(a,b) {
-    if (operator === "none") {
-        result = firstOperand
-        display.textContent = result
-    }
-    if (operator === "+") {
-        console.log(firstOperand + " " + operator + " " + secondOperand)
-        result = +a + +b
-    } else if(operator === "-") {
-        console.log(firstOperand + " " + operator + " " + secondOperand)
-        result = +a - +b
-    } else if(operator === "x") {
-        console.log(firstOperand + " " + operator + " " + secondOperand)
-        result = +a * +b
-    } else if(operator === "/") {
-        console.log(firstOperand + " " + operator + " " + secondOperand)
-        result = +a / +b
-    }
-    display.textContent = Math.round(result*10000000)/10000000
-    firstOperand = result
-    operator = "none"
-    secondOperand=""
-}
-
-function clearCalculator() {
-    operator = "none"
-    display.textContent = ""
-    firstOperand = ""
-    secondOperand = ""
-}
+/* Number button inputs */
 
 numberButtons.forEach(button => {
     button.addEventListener("click", function() {
@@ -84,6 +26,30 @@ numberButtons.forEach(button => {
     })
 })
 
+/* Big boy solve function */
+
+function solve(a,b) {
+    if (operator === "none") {
+        result = firstOperand
+        display.textContent = result
+    }
+    if (operator === "+") {
+        result = +a + +b
+    } else if(operator === "-") {
+        result = +a - +b
+    } else if(operator === "x") {
+        result = +a * +b
+    } else if(operator === "/") {
+        result = +a / +b
+    }
+    display.textContent = result
+    firstOperand = result
+    operator = "none"
+    secondOperand=""
+}
+
+/* Operator button */
+
 operatorButtons.forEach(button => {
     button.addEventListener("click", function() {
         if(operator === "none") {
@@ -94,7 +60,19 @@ operatorButtons.forEach(button => {
     })
 })
 
+/* Equals button */
+
 equalsButton.addEventListener("click", function() {
     solve(firstOperand,secondOperand)
 })
+
+/* Clear button */
+
+function clearCalculator() {
+    operator = "none"
+    display.textContent = ""
+    firstOperand = ""
+    secondOperand = ""
+}
+
 clearButton.addEventListener("click", clearCalculator)
